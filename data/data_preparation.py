c2706f2285c81f6c4feafc44c489e6204fd3dff0
dave117 <vedantdave77@gmail.com>
2022-04-30 23:18:13 -0400

    preparing data to feed into models

---
+++ data/data_preparation.py
@@ -0,0 +1,120 @@
+import numpy as np
+import os
+os.system('pip3 install category_encoder')
+from sklearn.preprocessing import OneHotEncoder
+
+from explore_data import reddit_test_numpy, reddit_train_numpy
+
+
+x_train = reddit_train_numpy[:,0]
+x_test = reddit_test_numpy[:,0]
+y_train = reddit_train_numpy[:,1]
+y_test = reddit_test_numpy[:,1]
+
+x = np.concatenate(reddit_train_numpy[:,0], reddit_test_numpy[:,0])
+print(x_train.shape, "+", x_test.shape, "=", x.shape)
+
+
+def get_num_classes(labels):
+    """
+    get total number of classes (logic)
+
+    # arguments
+     labels: list, label values
+     there shoud be at least one  example for the values in the range (0, num_classes -1)
+
+    # return
+        int, toal number of classes
+    
+    # Raises
+        ValueError: if any label value in the range (0,num_classes-1) is missing or if number of classes is <=1
+
+    """
+    num_classes = max(labels) + 1
+    missing_classes = [i for i in range(num_classes) if i not in labels]
+    if len(missing_classes):
+        raise ValueError('Missing sample with label value(s)' '{missing_classes}. Please make sure you hvae'
+        'at least one sample for every lable value ' 'in the ramge(0,{max_class})'.format(missing_classes=missing_classes, max_class=num_classes-1))
+
+    if num_classes <=1:
+        raise ValueError('Invalid number of lables: {num_classes}.' 'Please make sure there are at least two classes '
+        'of smaples'. format(num_classes = num_classes))
+        
+    return num_classes
+
+num_classes = get_num_classes(y_train)  # I add..
+
+
+# helper functions...
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+
+def ngram_vectorize(train_texts, train_labels, val_texts, MIN_DOCUMENT_FREQUENCY=2, TOKEN_MODE = 'word', NGRAM_RANGE=(1,2), TOP_K = 20000):
+    """Vectorizes tesxts as n-gram vectors...
+
+    text = tf-idf vector the lenght of vocabulary of unigrams + bigrams
+
+    # Arguments
+        train_texts = list, training text strings,
+        train_labels = np.ndarray,. training labels
+        val_texts = list, validation text strings
+        NGRAM_RANGE = Range (inclusive) of n-gram sizes for tokenizing texts
+        TOP_K = limit on the number of features
+        one of 'word', 'char', whether text should be split into word or characters n-gram
+        MIN_DOCUMENT_FREQUNCY = Min doc/corpus frequency below which a token will be discarded..
+
+    # Return
+        x_train, x_val = vectorized training and valication texts
+
+    """
+    # create teh key argumnet rules for the tf-idf vectorizer
+    kwargs = {
+        'ngram_range': NGRAM_RANGE, # USE 1-gram + 2-grams here in the alog...
+        'dtype': 'int32',
+        'strip_accents': 'unicode',
+        'decode_error' :'replace',
+        'analyzer': TOKEN_MODE, # split text into word token
+        'min_df' : MIN_DOCUMENT_FREQUENCY
+    }
+    vectorize = TfidfVectorizer(**kwargs) # load the rules...
+
+    # learn vocabular from training text and vectorize training texts
+    x_train = vectorize.fit_transform(train_texts)
+
+    # vectorize validation texts
+    x_val = vectorize.fit_transform(val_texts)
+
+    # select top 'k' on the vectorized features...
+    selector = SelectKBest(f_classif, k=min(TOP_K, x_train.shape[1]))
+    selector.fit(x_train, train_labels)
+    x_train = selector.transform(x_train).astype('float32')
+    x_val = selector.transform(x_val).astype('float32')
+    return x_train, x_val
+
+
+x_train_ngram, x_test_ngram = ngram_vectorize(x_train, x_test)
+print('x_train_ngram.shape= ', x_train_ngram.shape)
+print('x_test_ngram.shape= ', x_test_ngram.shape)
+
+ohe = OneHotEncoder(handle_unknown='ignore')
+y_train = ohe.fit_transform(reddit_train_numpy[:,1])
+y_test  = ohe.fit_transform(reddit_test_numpy[:,1])
+print(y_train)
+print("transformed into")
+print(reddit_train_numpy[0:4,1])
+print(reddit_train_numpy[13709, 13717,1])
+
+
+# rename input and labels...
+print(type(x_train_ngram))
+
+x_train_ngram = x_train_ngram.todense()
+print(type(x_train_ngram))
+print(x_train_ngram.shape)
+
+input_size = x_train_ngram.shape[1]
+
+
+
+
---
+++ data/explore_data.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import Counter
+from numpy import vectorize
+from phonenumbers import country_code_for_valid_region
+from sklearn.feature_extraction.text import CountVectorizer
+
+from load_data import reddit_train, reddit_test
+
+# clean up unnecessary columns
+reddit_train = reddit_train.drop(columns="Unnamed: 0")
+reddit_train = reddit_train.drop(columns="X")
+reddit_test = reddit_test.drop(columns="Unnamed: 0")
+reddit_test = reddit_test.drop(columns="X")
+
+
+# find gap between the data with different classes/labels
+gap_reddit_train= len(reddit_train[reddit_train['REMOVED'] == 0])- len(reddit_train[reddit_train['REMOVED'] == 1])
+print("reddit_train : 0 vs 1")
+print(len(reddit_train[reddit_train['REMOVED'] == 0])," vs ", len(reddit_train[reddit_train['REMOVED'] == 1])," = ", gap_reddit_train)
+print("")
+
+gap_reddit_test = len(reddit_test[reddit_test['REMOVED']== 0]) - len(reddit_test[reddit_test['REMOVED']== 1])
+print("reddit_test: 0 vs 1")
+print(len(reddit_test[reddit_test['REMOVED']== 0]), " vs ", len(reddit_test[reddit_test['REMOVED']== 1]))
+
+
+# convert to numpy array
+reddit_train_numpy = reddit_train.to_numpy()
+print('reddit_test_numpy_shape= ', reddit_train_numpy.shape)
+
+reddit_test_numpy = reddit_test.to_numpy()
+print('reddit_test_numpy= ', reddit_test_numpy.shape)
+
+
+#collect key matrix in trainng data...
+sample_texts = reddit_train_numpy[:,0]
+
+num_words = [len(s.split()) for s in sample_texts]
+
+print("max = ",np.max(num_words))
+print("min = ",np.min(num_words))
+print("median= ",np.median(num_words))
+
+
+# helper functions....
+def plot_frequency_distribution_of_ngrams(sample_texts,
+                                          ngram_range=(1, 2),
+                                          num_ngrams=50):
+    """Plots the frequency distribution of n-grams.
+
+    # Arguments
+        samples_texts: list, sample texts.
+        ngram_range: tuple (min, mplt), The range of n-gram values to consider.
+            Min and mplt are the lower and upper bound values for the range.
+        num_ngrams: int, number of n-grams to plot.
+            Top `num_ngrams` frequent n-grams will be plotted.
+    """
+    # Create args required for vectorizing.
+    kwargs = {
+            'ngram_range': (1, 1),
+            'dtype': 'int32',
+            'strip_accents': 'unicode',
+            'decode_error': 'replace',
+            'analyzer': 'word',  # Split text into word tokens.
+    }
+    vectorizer = CountVectorizer(**kwargs)
+
+    # This creates a vocabulary (dict, where keys are n-grams and values are
+    # idxices). This also converts every text to an array the length of
+    # vocabulary, where every element idxicates the count of the n-gram
+    # corresponding at that idxex in vocabulary.
+    vectorized_texts = vectorizer.fit_transform(sample_texts)
+
+    # This is the list of all n-grams in the index order from the vocabulary.
+    all_ngrams = list(vectorizer.get_feature_names())
+    num_ngrams = min(num_ngrams, len(all_ngrams))
+    # ngrams = all_ngrams[:num_ngrams]
+
+    # Add up the counts per n-gram ie. column-wise
+    all_counts = vectorized_texts.sum(axis=0).tolist()[0]
+
+    # Sort n-grams and counts by frequency and get top `num_ngrams` ngrams.
+    all_counts, all_ngrams = zip(*[(c, n) for c, n in sorted(
+        zip(all_counts, all_ngrams), reverse=True)])
+    ngrams = list(all_ngrams)[:num_ngrams]
+    counts = list(all_counts)[:num_ngrams]
+
+    idx = np.arange(num_ngrams)
+    plt.bar(idx, counts, width=0.8, color='b')
+    plt.xlabel('N-grams')
+    plt.ylabel('Frequencies')
+    plt.title('Frequency distribution of n-grams')
+    plt.xticks(idx, ngrams, rotation=45)
+    plt.show()
+
+
+def plot_sample_length_distribution(sample_texts):
+    """
+    plots the sample length distribution...
+
+    #Arguments:
+        sample_texts: list, sample texts
+
+    # return
+    plot the sample distribution
+    """
+    plt.hist([len(s) for s in sample_texts], 50)
+    plt.xlabel("Length of a sample")
+    plt.ylabel("Number of sample")
+    plt.title("Sample length distribution")
+    plt.show()
+
+
+plot_frequency_distribution_of_ngrams(sample_texts, ngram_range=(1,2), num_ngrams=50)
+plot_sample_length_distribution(sample_texts)
+
+
+# calculate ratio for the model selection
+ratio = reddit_train_numpy.shape[0]/ np.median(num_words)
+print(ratio)
