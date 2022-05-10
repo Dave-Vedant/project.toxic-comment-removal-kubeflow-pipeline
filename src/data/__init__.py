823163dc4a54f51d6c79e42bbeb912c9fe11e659
dave117 <vedantdave77@gmail.com>
2022-05-10 14:21:22 -0400

    minor chagnes

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,10 +1,15 @@
 import sys
 sys.path.append('./src/data') # set package directory to subdirectory (one level above the .py file)
-import data_preparation
 
+# implicite importing
 
-# simplifying the importing ..
+# importing prepared data for models...
 from .data_preparation import x_train, x_test, y_test, y_train, x_test_ngram, x_train_ngram, input_size
 
+
+# numpy conversion of dataset...
 from .explore_data import reddit_test_numpy, reddit_train_numpy 
+
+# load main dataset...
+from .load_data import reddit_train, reddit_test, reddit_200k_test, reddit_200k_train
 
